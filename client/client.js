a05ecce5176cdbd9b157c09d42a14c11665ea799
Ben-Arlanda <ben.arlanda@protonmail.com>
2024-02-15 12:49:26 +1100

    added dark mode feature

--- client/client.js
+++ client/client.js
@@ -463,3 +463,11 @@ function findNearestStations(lat, lng, radius) {
             }
         })
 }
+
+
+const darkModeToggle = document.getElementById('darkModeToggle')
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode')
+})
